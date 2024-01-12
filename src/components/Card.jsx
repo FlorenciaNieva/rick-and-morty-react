bc80298c8ad24f34b54f90e683aa6bafcdcdcf70
FlorenciaNieva <florencianieva930@gmail.com>
2024-01-12 15:12:13 -0300

    Incorporó estilos a las tarjetas en el contenedor al pasar el cursor por encima

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,38 @@
+import { useState } from "react";
 import { Card as CustomCard, Row, Col } from "react-bootstrap";
 import { useNavigate } from "react-router-dom"
 
 function Card({ id, name, image, status, species, gender }) {
   const details = [status, species, gender];
   const navigate = useNavigate();
+  const [hovered, setHovered] = useState(false);
+
+  const handleHover = () => {
+    setHovered(true);
+  };
+
+  const handleLeave = () => {
+    setHovered(false);
+  };
+
+  const cardStyle = {
+    width: "33vw",
+    margin: "10px",
+    border: '1px solid #ccc',
+    transition: 'transform 0.3s, box-shadow 0.3s',
+    transform: hovered ? 'scale(1.1)' : 'scale(1)',
+    boxShadow: hovered ? '0 0 10px rgba(0, 0, 0, 0.5)' : 'none',
+    cursor: 'pointer',
+  };
   
   return (
-    <CustomCard key={id} style={{ width: "33vw", margin: "5px" }} onClick={() => navigate(`/details/${id}`)}>
+    <CustomCard 
+      key={id} 
+      style={cardStyle} 
+      onClick={() => navigate(`/details/${id}`)}
+      onMouseEnter={handleHover}
+      onMouseLeave={handleLeave}
+    >
       <CustomCard.Body>
         <Row>
           <Col className="d-flex align-items-center justify-content-center">
